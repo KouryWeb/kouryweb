30d2da72282c56ec8e458fd1ed94300126f1462c
Philip Koury <phil@kouryweb.com>
2024-07-27 12:00:27 -0400

    Tweak Why Us styling.

--- resources/views/partials/why-us.blade.php
+++ resources/views/partials/why-us.blade.php
@@ -1,31 +1,29 @@
 <div id="why-us" class="why-us">
     <div class="container sec2 mw1600">
-        <div class="row justify-content-start align-items-center">
-            <div class="col-xl-7 col-lg-12">
-                <h3>Why Choose</h3>
-                <h3 class="hellix-black-ss11">Koury <span class="prp">Web</span></h3>
-                <h4 class="mt20 prp">Its all in the details.</h4>
-            </div>
+        <div class="justify-content-start align-items-center">
+            <h3>Why Choose</h3>
+            <h3 class="hellix-black-ss11">Koury <span class="prp">Web</span></h3>
+            <h4 class="mt20 prp">Its all in the details.</h4>
         </div>
 
         <div class="row mt60 tb">
-            <div class="col-xl-1 col-lg-1 text-center tb1">
+            <div class="col-md-1 text-center tb1">
                 <lord-icon src="https://cdn.lordicon.com/iazmohzf.json" trigger="hover" delay="2000" colors="primary:#fff" style="width:50px;height:50px"></lord-icon>
             </div>
-            <div class="col-xl-5 col-lg-5 tb2">
+            <div class="col-md-5 tb2">
                 <h4 class="prp">Web Software Artisans</h4>
-                <ul class="mt-20 text-secondary">
+                <ul class="mt-20">
                     <li>Develop ground-up custom applications</li>
                     <li>Integrate your business systems with nearly any external service</li>
                 </ul>
             </div>
 
-            <div class="col-xl-1 col-lg-1 text-center tb1 tb1l">
+            <div class="col-md-1 text-center tb1 tb1l">
                 <lord-icon src="https://cdn.lordicon.com/qnpnzlkk.json" trigger="hover" delay="2000" colors="primary:#fff" style="width:50px;height:50px"></lord-icon>
             </div>
-            <div class="col-xl-5 col-lg-5 tb2">
+            <div class="col-md-5 tb2">
                 <h4 class="prp">Design Meets Functionality</h4>
-                <ul class="mt-20 text-secondary">
+                <ul class="mt-20">
                     <li>Tailoring visually stunning, user-friendly, and responsive designs</li>
                     <li>Balancing design and robust engineering optimal user experience</li>
                 </ul>
@@ -34,23 +32,23 @@
 
 
         <div class="row tb">
-            <div class="col-xl-1 col-lg-1 text-center tb1">
+            <div class="col-md-1 text-center tb1">
                 <lord-icon src="https://cdn.lordicon.com/uwinmnkh.json" trigger="hover" delay="2000" colors="primary:#fff" style="width:50px;height:50px"></lord-icon>
             </div>
-            <div class="col-xl-5 col-lg-5 tb2">
+            <div class="col-md-5 tb2">
                 <h4 class="prp">Streamline Your Operations</h4>
-                <ul class="mt-20 text-secondary">
+                <ul class="mt-20">
                     <li>Our unique business process knowledge accelerates your growth</li>
                     <li>Transform operations with seamless efficiency</li>
                 </ul>
             </div>
 
-            <div class="col-xl-1 col-lg-1 text-center tb1 tb1l">
+            <div class="col-md-1 text-center tb1 tb1l">
                 <lord-icon src="https://cdn.lordicon.com/sbrtyqxj.json" trigger="hover" delay="2000" colors="primary:#fff" style="width:50px;height:50px"></lord-icon>
             </div>
-            <div class="col-xl-5 col-lg-5 tb2">
+            <div class="col-md-5 tb2">
                 <h4 class="prp">Empowering Your Brand</h4>
-                <ul class="mt-20 text-secondary">
+                <ul class="mt-20">
                     <li>Elevate your brand with our creative design expertise</li>
                     <li>Delivering designs that captivate and communicate effectively</li>
                 </ul>
@@ -58,23 +56,23 @@
         </div>
 
         <div class="row tb tb0">
-            <div class="col-xl-1 col-lg-1 text-center tb1">
+            <div class="col-md-1 text-center tb1">
                 <lord-icon src="https://cdn.lordicon.com/hrjifpbq.json" trigger="hover" delay="2000" colors="primary:#fff" style="width:50px;height:50px"></lord-icon>
             </div>
-            <div class="col-xl-5 col-lg-5 tb2">
+            <div class="col-md-5 tb2">
                 <h4 class="prp">Collaborative Innovation</h4>
-                <ul class="mt-20 text-secondary">
+                <ul class="mt-20">
                     <li>We care about your needs and explain how our solutions will help</li>
                     <li>Develop solutions that specifically cater to your workflows</li>
                 </ul>
             </div>
 
-            <div class="col-xl-1 col-lg-1 text-center tb1 tb1l">
+            <div class="col-md-1 text-center tb1 tb1l">
                 <lord-icon src="https://cdn.lordicon.com/ulnswmkk.json" trigger="hover" delay="2000" colors="primary:#fff" style="width:50px;height:50px"></lord-icon>
             </div>
-            <div class="col-xl-5 col-lg-5 tb2">
+            <div class="col-md-5 tb2">
                 <h4 class="prp">Partner With Us</h4>
-                <ul class="mt-20 text-secondary">
+                <ul class="mt-20">
                     <li>We have been growing with our clients since 2004</li>
                     <li>Propel your business with software and designs built just for you</li>
                 </ul>
